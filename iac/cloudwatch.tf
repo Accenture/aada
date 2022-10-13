b1f62b86d5e88336eedc470cae14986250fd2869
Eric Hill <eric.hill@accenture.com>
2022-10-13 10:34:22 -0500

    Adjusted retention times for CloudWatch logs now that we have AUDIT events

--- iac/cloudwatch.tf
+++ iac/cloudwatch.tf
@@ -5,17 +5,17 @@ resource "aws_cloudwatch_log_group" "apigw_ws" {
 
 resource "aws_cloudwatch_log_group" "apigw_http" {
   name              = "/aws/apigateway/${aws_apigatewayv2_api.httpapi.id}/${aws_apigatewayv2_stage.httpapi_prod_stage.name}"
-  retention_in_days = 30
+  retention_in_days = 5
 }
 
 resource "aws_cloudwatch_log_group" "lambda_ws" {
   name              = "/aws/lambda/${aws_lambda_function.ws.function_name}"
-  retention_in_days = 3
+  retention_in_days = 5
 }
 
 resource "aws_cloudwatch_log_group" "lambda_http" {
   name              = "/aws/lambda/${aws_lambda_function.http.function_name}"
-  retention_in_days = 3
+  retention_in_days = 30
 }
 
 resource "aws_cloudwatch_log_metric_filter" "throttle_filter" {
